aa24c01d3ec5c9043d545710b883c1eaee504c9d
Noah A <nanderson815@gmail.com>
2019-02-18 14:36:24 -0500

    changed port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var path = require('path');
 var app = express();
 
-var PORT = $port;
+var PORT = 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
